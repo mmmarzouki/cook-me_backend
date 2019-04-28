d47c859fb7fd693883aa21bf950f0fb4ef44b478
mmmarzouki <mmmarzouki@gmail.com>
2019-04-28 14:15:25 +0200

    added step model and repository

---
+++ src/main/java/insat/gl4/cookme/models/Step.java
@@ -0,0 +1,23 @@
+package insat.gl4.cookme.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Step {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id;
+
+    @Column(name = "value")
+    private String value;
+}
---
+++ src/main/java/insat/gl4/cookme/repositories/StepRepository.java
@@ -0,0 +1,7 @@
+package insat.gl4.cookme.repositories;
+
+import insat.gl4.cookme.models.Step;
+import org.springframework.data.repository.CrudRepository;
+
+public interface StepRepository extends CrudRepository<Step, Integer> {
+}
